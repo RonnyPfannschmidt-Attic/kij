e213eeda5b04725357adee5941fdcce6b797303e
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-02-21 22:21:07 +0100

    add a simple pth file reader/writer (no inline code)

---
+++ pu/files/pth.py
@@ -0,0 +1,39 @@
+'''
+    kid.files.pth
+    ~~~~~~~~~~~~~
+
+    deals with reading, editing and writing pth files
+
+    :license: GNU GPL3
+    :copyright: 2010 by Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
+'''
+from py.path import local
+
+class PthFile(object):
+    def __init__(self, path):
+        self.path = local(path)
+        self.entries = []
+        if not self.path.check():
+            return
+        with self.path.open() as f:
+            for entry in f:
+                entry = entry.strip()
+                if entry[0] == '.':
+                    self.append(self.path.dirpath().join(entry))
+                else:
+                    self.append(local(entry))
+
+    def append(self, path):
+        if path not in self.entries:
+            self.entries.append(path)
+
+    def save(self):
+        with self.path.open('w') as f:
+            for path in self.entries:
+                rel = path.relto(self.path.dirpath())
+                if rel:
+                    f.write('./%s\n'%rel)
+                else:
+                    f.write('%s\n'%path)
+
+
---
+++ tests/files/test_pth.py
@@ -0,0 +1,52 @@
+import py
+from pu.files.pth import PthFile
+
+def pytest_funcarg__pthfile(request):
+    return request.getfuncargvalue('tmpdir').join('test.pth')
+
+def pytest_funcarg__pth(request):
+    return PthFile(request.getfuncargvalue('pthfile'))
+
+def pytest_funcarg__item(request):
+    tmpdir = request.getfuncargvalue('tmpdir')
+    return tmpdir.join('test-1.3.zip')
+
+def test_instanciate(tmpdir):
+    PthFile(tmpdir.join('test.pthfail'))
+
+def test_create_pth(tmpdir):
+    file = tmpdir.join('test.pth')
+    pth = PthFile(file)
+    assert not pth.entries
+
+def test_append(pth, tmpdir, item):
+
+    pth.append(item)
+    assert pth.entries == [item]
+
+    # dont get double entries
+    pth.append(item)
+    assert pth.entries == [item]
+
+def test_save_relative(pth, pthfile, tmpdir, item):
+    pth.append(item)
+    pth.save()
+    assert pthfile.read() == './test-1.3.zip\n'
+
+def test_save_absolute(pth, pthfile):
+    pth.append(py.path.local('/usr/local/lib'))
+    pth.save()
+    assert pthfile.read() == '/usr/local/lib\n'
+
+
+def test_parse_relative(pthfile, item):
+    pthfile.write('./test-1.3.zip')
+    pth = PthFile(pthfile)
+    assert pth.entries == [item]
+
+def test_parse_absolute(pthfile):
+    pthfile.write('/usr/local/bin/test')
+    pth = PthFile(pthfile)
+    assert pth.entries == ['/usr/local/bin/test']
+
+

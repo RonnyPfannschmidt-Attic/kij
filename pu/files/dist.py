376d6128387f76b20b61c3855811c3e65d7f49a9
Ali Afshar <aafshar@gmail.com>
2010-02-23 14:31:44 +0000

    Dist file support

---
+++ pu/files/dist.py
@@ -0,0 +1,87 @@
+"""
+    pu.files.dist
+    ~~~~~~~~~~~~~
+
+    Read and write pu distribution files.
+
+    :license: MIT/PSF (see LICENSE.txt file)
+    :copyright: pu Authors (see AUTHORS.txt file)
+
+    This is the primary configuration file for any Python distribution. It
+    aims to capture all the data required for distribution. The dist file is
+    presented as a Yaml file, with a number of keys, each corresponding to a
+    configuration directive for the distribution.
+
+    A very simple example can be seen::
+
+    name:    Blogatron
+    version: 1.0
+    author:  Ali Afshar
+    packages:
+        - blogatron
+
+    Although the specification will grow, certain fields are recognised, some
+    are required, and some optional. Other when present need to be of a
+    certain type. The recognised attributes are desribed:
+
+    name
+        The distribution name, usually the name of the project. *required*
+
+    version
+        The version of the distribution, should be a string of the numerical
+        version number.
+
+    summary
+        A short one-line summary of the distribution.
+
+    packages
+        A list of package descriptions to add to the distribution
+
+
+    Being Yaml, there is a certain degree of type expressivity over ini-based
+    systems. Addiitonally, the Yaml loading layer can be easily swapped out
+    for anything else.
+"""
+
+import yaml
+
+
+from py.path import local
+
+
+DIST_FILE_KEYS = set((
+    'packages', 'summary',
+))
+
+
+class DistFileError(ValueError):
+    """Error with Dist File"""
+
+
+class DistFile(object):
+
+    def __init__(self, path):
+        self.path = local(path)
+        self._load()
+
+    def _load(self):
+        # XXX Handle exceptions
+        try:
+            conf = yaml.load(self.path.open())
+        except Exception, e:
+            raise DistFileError('Error loading file %s %s' % (self.path, e))
+        self.name = conf.get('name')
+        if self.name is None:
+            raise DistFileError('Missing "name" key %s' % self.path)
+        # now the optional keys
+        for k in DIST_FILE_KEYS:
+            setattr(self, k, conf.get(k))
+        # now some validation
+        if self.packages and not isinstance(self.packages, list):
+            raise DistFileError('packages must be a list')
+        self._model = conf
+
+    def __str__(self):
+        return "<DistFile: %s>" % self.path
+
+
---
+++ tests/files/test_dist.py
@@ -0,0 +1,61 @@
+
+import py
+
+from pu.files.dist import DistFile, DistFileError
+
+def pytest_funcarg__distfile(request):
+    return request.getfuncargvalue('tmpdir').join('test-kij.yml')
+
+def pytest_funcarg__distfilegen(request):
+    distfile = request.getfuncargvalue('distfile')
+    def _create_file(s, distfile=distfile):
+        f = distfile.open('w')
+        f.write(s)
+        f.close()
+        return DistFile(distfile)
+    return _create_file
+
+d = """
+name: Kij
+summary: Python packaging utilities
+packages:
+    - anyvc.*
+"""
+
+def test_name(distfilegen):
+    df = distfilegen(d)
+    assert df.name == 'Kij'
+
+e = """
+packages:
+    - anyvc.*
+"""
+
+def test_missing_name(distfilegen):
+    py.test.raises(DistFileError, distfilegen, e)
+
+def test_summary(distfilegen):
+    df = distfilegen(d)
+    assert df.summary == 'Python packaging utilities'
+
+def test_packages(distfilegen):
+    df = distfilegen(d)
+    assert df.packages[0] == 'anyvc.*'
+    assert len(df.packages) == 1
+
+f = """
+name: banana
+"""
+
+def test_missing_packages_ok(distfilegen):
+    df = distfilegen(f)
+    assert df.packages is None
+
+g = """
+name: banana
+packages: manana
+"""
+
+def test_bad_packages(distfilegen):
+    py.test.raises(DistFileError, distfilegen, g)
+

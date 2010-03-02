8575c126e2797feab9d542ba68be1badfe4ce368
Ali Afshar <aafshar@gmail.com>
2010-03-02 23:47:56 +0000

    Added a build command

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,17 @@ def pytest_funcarg__source(request):
     tmpdir = request.getfuncargvalue('tmpdir')
     return tmpdir.ensure('source', dir=True)
 
+def pytest_funcarg__fullsource(request):
+    source = request.getfuncargvalue('source')
+    source.join('kij.yml').write(
+            'name: test\n'
+            'scripts: [foo, bin/bar]\n'
+            'packages: [testpkg]\n'
+            )
+    source.ensure('foo')
+    source.ensure('bin/bar')
+    source.ensure('testpkg/__init__.py')
+    return source
 
 def pytest_funcarg__config(request):
     source = request.getfuncargvalue('source')
